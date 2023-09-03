27391d0a309811ece3537a8b776abf52deb264d8
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-02 23:25:09 -0300

    config: Jest and IDE configurations added to project

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import {
-  NestFastifyApplication,
-  FastifyAdapter,
-} from '@nestjs/platform-fastify';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
-  );
-  await app.listen(3000, '0.0.0.0');
+  )
+  await app.listen(3000, '0.0.0.0')
 }
-bootstrap();
+bootstrap()
