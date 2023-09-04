9cc9a712f6e78643a986520e6c6258bfca06c889
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-04 15:20:00 -0300

    test: ensure UserEntity has getters and work properly with defined values, correct values and correct type of the values

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -17,10 +17,30 @@ describe('UserEntity unit tests', () => {
 
     sut = new UserEntity(props)
   })
-  it('Constructor method', () => {
+  it('should have correct values', () => {
     expect(sut.props.name).toBe(props.name)
     expect(sut.props.email).toBe(props.email)
     expect(sut.props.password).toBe(props.password)
     expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+
+  it('should have getters for props ', () => {
+    expect(sut.props.name).toBeDefined()
+    expect(sut.props.email).toBeDefined()
+    expect(sut.props.password).toBeDefined()
+    expect(sut.props.createdAt).toBeDefined()
+  })
+
+  it('getter should have correct values ', () => {
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+
+  it('getters should have correct type values', () => {
+    expect(typeof sut.props.name).toBe('string')
+    expect(typeof sut.props.email).toBe('string')
+    expect(typeof sut.props.password).toBe('string')
   })
 })
