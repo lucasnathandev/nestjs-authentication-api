abdc8558a4dae522792de7d28db7c143a07e6a46
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-05 22:55:09 -0300

    test: ensure ClassValidatorFields initializes erros and validatedData with null and stub simulation with error, makes validate method to record the error into errors object

--- src/shared/domain/validators/class-validator-fields.ts
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -9,6 +9,7 @@ export abstract class ClassValidatorFields<PropsValidated>
   validate(data: any): boolean {
     const errors = validateSync(data)
     if (errors.length) {
+      this.errors = {}
       errors.forEach(error => {
         const field = error.property
         this.errors[field] = Object.values(error.constraints)
