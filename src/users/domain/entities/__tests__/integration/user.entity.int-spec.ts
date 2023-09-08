d8976512140340cbf16d5171c2b8c6127ade5f3c
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-08 15:17:48 -0300

    test: ensure UserEntity throws error when creation date (createdAt) data is invalid

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -93,5 +93,21 @@ describe('UserEntity integration tests', () => {
 
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
     })
+
+    it('should throw an error when creating a user with invalid createdAt', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        createdAt: 10 as any,
+      }
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        createdAt: 'wednesday' as any,
+      }
+
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+    })
   })
 })
