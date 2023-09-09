aac8618c2aebd7af5b8fc9e461fdb696af0d5ba0
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-09 20:23:04 -0300

    test: ensure InMemoryRepository throws error when trying to find entity by not found id and returns the entity when valid id

--- src/shared/domain/repositories/__tests__/unit/in-memory-repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-repository.spec.ts
@@ -1,5 +1,6 @@
 import { Entity } from '@/shared/domain/entities/entity'
 import { InMemoryRepository } from '../../in-memory.repository'
+import { NotFoundError } from '@/shared/domain/errors/notfound-error'
 
 type StubEntityProps = {
   name: string
@@ -25,5 +26,21 @@ describe('InMemoryRepository unit tests', () => {
     await sut.insert(entity)
 
     expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
+  })
+
+  it('should throw error when entity not found', async () => {
+    await expect(sut.findById('fakeid')).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    )
+  })
+
+  it('should find a new entity in repository', async () => {
+    const entity = new StubEntity({
+      name: 'test',
+      price: 40,
+    })
+    await sut.insert(entity)
+    const result = await sut.findById(entity.id)
+    expect(entity.toJSON()).toStrictEqual(result.toJSON())
   })
 })
