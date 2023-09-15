6fb17684a00a1e713a109cc782d3f3cbe0140d2d
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-14 23:31:35 -0300

    test: ensure applyFilter method in InMemorySearchableRepository works correctly

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable-repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable-repository.spec.ts
@@ -30,7 +30,47 @@ describe('InMemoryRepository unit tests', () => {
   })
 
   describe('applyFilter method', () => {
-    it('', async () => {})
+    it('should no filter items when filter param is null', async () => {
+      const items = [
+        new StubEntity({
+          name: 'bread',
+          price: 10,
+        }),
+      ]
+
+      const spyFilterMethod = jest.spyOn(items, 'filter')
+
+      const itemsFiltered = await sut['applyFilter'](items, null)
+
+      expect(itemsFiltered).toStrictEqual(items)
+      expect(spyFilterMethod).not.toHaveBeenCalled()
+    })
+
+    it('should filter using a filter param', async () => {
+      const items = [
+        new StubEntity({ name: 'bread', price: 10 }),
+        new StubEntity({ name: 'Knife', price: 20 }),
+        new StubEntity({ name: 'knife', price: 20 }),
+        new StubEntity({ name: 'apple', price: 5 }),
+      ]
+
+      const spyFilterMethod = jest.spyOn(items, 'filter')
+
+      let itemsFiltered = await sut['applyFilter'](items, 'Knife')
+
+      expect(itemsFiltered).toStrictEqual([items[1], items[2]])
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1)
+
+      itemsFiltered = await sut['applyFilter'](items, 'knife')
+
+      expect(itemsFiltered).toStrictEqual([items[1], items[2]])
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2)
+
+      itemsFiltered = await sut['applyFilter'](items, 'unexisting filter value')
+
+      expect(itemsFiltered).toHaveLength(0)
+      expect(spyFilterMethod).toHaveBeenCalledTimes(3)
+    })
   })
   describe('applySort method', () => {})
   describe('applyPaginate method', () => {})
